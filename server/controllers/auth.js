3260b57350e41e025dc3516c22cbd6874b866946
adityaVelhal <aditya.v.velhal@gmail.com>
2023-09-20 22:12:35 +0530

    Completed Signup and Signin

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,10 +1,38 @@
 import mongoose from 'mongoose';
 import Users from '../models/Users.js';
+import bcrypt from 'bcrypt';
+import { createError } from '../error.js';
+import jwt from 'jsonwebtoken';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     try {
-        const newUser = new Users(req.body);
+        let hash = await bcrypt.hash(req.body.password, 10);
+        const newUser = new Users({ ...req.body, password: hash });
+        await newUser.save();
+        res.status(200).send('User has been created!');
     } catch (err) {
-        throw err;
+        next(err);
+    }
+};
+
+export const signin = async (req, res, next) => {
+    try {
+        const user = await Users.findOne({ email: req.body.email });
+        if (!user) {
+            next(createError(400, 'Incorrect credentials.1'));
+        }
+
+        const isCorrect = bcrypt.compare(req.body.password, user.password);
+        if (!isCorrect) next(createError(400, 'Incorrect credentials'));
+
+        const token = await jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+        res.cookie('access_token', token, {
+            httpOnly: true,
+        })
+            .status(200)
+            .json({ ...user._doc, password: null });
+    } catch (err) {
+        next(err);
     }
 };
--- server/index.js
+++ server/index.js
@@ -9,10 +9,23 @@ import authRoutes from './routes/auth.js';
 const app = express();
 dotenv.config();
 
+app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/video', videoRoutes);
 app.use('/api/comments', commentRoutes);
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const msg = err.message || 'Something went wrong';
+
+    return res.status(status).json({
+        success: false,
+        status,
+        message: msg,
+    });
+});
 
 const conn = () => {
     mongoose
